c506e8ebd1bdb37b3fa0baee116cd54e64ee3351
bodhichristian <clavelle23@gmail.com>
2024-08-21 07:17:52 -0400

    add environment to previews to prevent crashing

--- SpiralNotes/ContentView.swift
+++ SpiralNotes/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
@@ -55,5 +56,6 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .modelContainer(try! ModelContainer(for: Note.self))
 }
 
