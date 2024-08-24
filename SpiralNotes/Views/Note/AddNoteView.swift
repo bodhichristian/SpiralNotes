6e0209634bb5a2b76599dc94607d2b89347e8738
bodhichristian <clavelle23@gmail.com>
2024-08-23 20:39:22 -0400

    complete major ui experiences

--- SpiralNotes/Views/Note/AddNoteView.swift
+++ SpiralNotes/Views/Note/AddNoteView.swift
@@ -14,9 +14,10 @@ struct AddNoteView: View {
     @State private var noteContent: String = ""
     @State private var noteTitle: String = ""
     @State private var noteColor: String = "blue"
+    @State private var didEditNote: Bool = false
     
     var body: some View {
-        ZStack(alignment: .bottomTrailing) {
+        ZStack {
             LinearGradient(
                 colors: [
                     Color(colorName: noteColor)!.opacity(0.1),
@@ -28,45 +29,70 @@ struct AddNoteView: View {
                 endPoint: .bottom
             )
             .ignoresSafeArea()
-            
-            // Note Contents
-            ScrollView {
-                VStack(alignment: .leading, spacing: 0) {
-                    TextField("Note Title", text: $noteTitle)
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .foregroundStyle(.primary)
-                        .padding()
+            VStack {
+                ZStack {
+                    // Note Contents
                     
-                    TextEditor(text: $noteContent)
-                        .textEditorStyle(.plain)
-                        .focused($isEditingNote)
-                        .padding(.horizontal)
+                        VStack(alignment: .leading, spacing: 0) {
+                            TextField("Note Title", text: $noteTitle)
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .foregroundStyle(.primary)
+                                .padding(.leading)
+                                .padding(.top, 40)
+                                .onTapGesture {
+                                    withAnimation {
+                                        didEditNote = true
+                                    }
+                                }
+                            
+                            TextEditor(text: $noteContent)
+                                .textEditorStyle(.plain)
+                                .focused($isEditingNote)
+                                .padding(.horizontal)
+                                .onAppear {
+                                    isEditingNote = true
+                                }
+                                .onTapGesture {
+                                    withAnimation {
+                                        didEditNote = true
+                                    }
+                                }
+                                
+                        }
+                        .frame(maxHeight: .infinity, alignment: .top)
+                        
+                    
+                    
+                    HStack(alignment: .bottom) {
+                        NotebookButton(symbolName: "trash.circle.fill") {
+                            dismiss()
+                        }
+                        
+                        ColorSelectionCapsule(colorName: $noteColor)
+                            
+                        
+                        NotebookButton(symbolName: "checkmark.circle.fill") {
+                            
+                        }
+                    }
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                    .padding(.bottom)
                 }
+                //.clipShape(RoundedRectangle(cornerRadius: 10))
+                //.frame(maxWidth: .infinity, maxHeight: 400)
+                .padding(.horizontal)
+                
+//                if !didEditNote{
+//                    Text("\(Text("Thoughts").foregroundStyle(Color(colorName: noteColor)!)): e n d l e s s")
+//                        .font(.title3)
+//                        .fontWeight(.medium)
+//                }
             }
             
-            HStack(alignment: .bottom) {
-                NotebookButton(symbolName: "trash.circle.fill") {
-                    dismiss()
-                }
-                
-                ColorSelectionCapsule(colorName: $noteColor)
-                    
-                
-                NotebookButton(symbolName: "checkmark.circle.fill") {
-                    
-                }
-            }
-            .font(.largeTitle)
-            .frame(maxWidth: .infinity)
-            .padding(.bottom)
         }
-        .onAppear {
-            isEditingNote = true
-        }
-        .clipShape(RoundedRectangle(cornerRadius: 10))
-        .frame(maxWidth: .infinity, maxHeight: 400)
-        .padding(.horizontal)
+        
     }
 }
 
--- SpiralNotes/Views/Sticky/AddStickyView.swift
+++ SpiralNotes/Views/Sticky/AddStickyView.swift
@@ -9,29 +9,55 @@ import SwiftUI
 
 struct AddStickyView: View {
     @Environment(\.dismiss) private var dismiss
-    @State private var newSticky = Sticky(title: "New Sticky", content: "Write here.", color: "yellow")
+    @State private var newSticky = Sticky(title: "New Sticky", content: "Write here...", color: "yellow")
     
-    @State private var stickyColor = "yellow"
+    @State private var stickyColor = "green"
+    @State private var includingDate = true
+    @State private var newStickyTitle = ""
+    @State private var didEditSticky: Bool = false
+    
+    let offset: CGFloat = -30
     
     var body: some View {
-        VStack {
+        VStack(spacing: 40) {
+            
+            if !didEditSticky {
+                Text("\(Text("Title").foregroundStyle(Color(colorName: stickyColor)!)) and \(Text("date").foregroundStyle(Color(colorName: stickyColor)!)) optional")
+                    .font(.title3)
+                    .fontWeight(.medium)
+            }
+            
             ZStack(alignment: .bottom) {
                 
                 RoundedRectangle(cornerRadius: 10)
                     .foregroundStyle(Color(colorName: stickyColor)!.gradient)
-                    .frame(maxWidth: 320, maxHeight: 320)
+                    .frame(maxWidth: 300, maxHeight: 300)
                     .shadow(radius: 4, y: 4)
                     .overlay {
                         VStack(alignment: .leading, spacing: 4) {
-                            TextField("Sticky Title", text: $newSticky.title)
+                            TextField("New Sticky", text: $newStickyTitle)
                                 .foregroundStyle(.black)
                                 .font(.title)
                                 .fontWeight(.semibold)
+                                .onTapGesture {
+                                    didEditSticky = true
+                                }
                             
-                            Text(Date.now.formatted(date: .numeric, time: .omitted))
-                                .fontWeight(.semibold)
-                                .foregroundStyle(.black)
-                                .padding(.bottom, 4)
+                            HStack {
+                                if includingDate {
+                                    Text(Date.now.formatted(date: .numeric, time: .omitted))
+                                        .fontWeight(.semibold)
+                                        .foregroundStyle(.black)
+                                        .padding(.bottom, 4)
+                                }
+                                
+                                Toggle(includingDate ? "X Date" : "+ Date", isOn: $includingDate.animation())
+                                    .toggleStyle(.button)
+                                    .font(.caption.bold())
+                                    .tint(.black)
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                            }
+                            .offset(y: includingDate ? 0 : offset)
                             
                             TextEditor(text: $newSticky.content)
                                 .textEditorStyle(.plain)
@@ -39,6 +65,12 @@ struct AddStickyView: View {
                                 .font(.subheadline)
                                 .foregroundStyle(.black)
                                 .padding(-4)
+                                .offset(y: includingDate ? 0 : offset)
+                                .onTapGesture {
+                                    withAnimation {
+                                        didEditSticky = true
+                                    }
+                                }
                         }
                         .frame(
                             maxWidth: .infinity,
@@ -47,21 +79,27 @@ struct AddStickyView: View {
                         )
                         .padding()
                     }
+                
                 HStack(alignment: .bottom) {
                     NotebookButton(symbolName: "trash.circle.fill") {
                         dismiss()
                     }
                     
                     ColorSelectionCapsule(colorName: $stickyColor)
-                    
+                
                     NotebookButton(symbolName: "checkmark.circle.fill") {
-                        
+                        // Save notebook
                     }
                 }
                 .font(.largeTitle)
                 .padding(.bottom)
             }
             
+            if !didEditSticky {
+                Text("Stickiness \(Text("mandatory").foregroundStyle(Color(colorName: stickyColor)!))")
+                    .font(.title3)
+                    .fontWeight(.medium)
+            }
         }
     }
 }
